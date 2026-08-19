87941455cf79cfb41426077cc91d4fd434c4215c
jonwhoo <107388668+jonwhoo@users.noreply.github.com>
2026-08-19 17:41:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# 描述: 编译前引入第三方软件源 (Feeds)
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 1. OpenClash
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 2. PassWall 及其依赖包
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
-
-# 3. SSR+ (helloworld)
-echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >> feeds.conf.default
+# Add feed sources
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
